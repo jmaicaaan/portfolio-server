1ce9419e359cc3252ed9e67908d62ebd10b63ebe
jmaicaaan <jmsantosss29@yahoo.com>
2017-11-10 23:55:16 +0800

    Wip promise

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const githubAPI = require('github');
 const cors = require('cors');
+const promise = require('bluebird');
 const github = new githubAPI({});
 
 const app = express();
 const port = process.env.PORT || 3000
+
+github.authenticate({
+  type: 'token',
+  token: '35a85f5f8ff48f417b51ef7ff0e5c5c2001351c9' //restrict access
+});
 
 app.use(cors());
 
@@ -26,20 +32,36 @@ app.get('/repo/:username', (req, res) => {
 
 function getBasicDetails(data) {
   if (data && data.length) {
-    data = data.map((repos) => {
-      if (!repos.fork) {
-        return {
-          name: repos.name,
-          description: repos.description, 
-          url: repos.html_url,
-        }
-      }
-    }).filter((repos) => {
-      return repos && Object.keys(repos).length
+    return promise.map(data, (repository) => {
+      return getRepoLanguage(repository.name)
+        .then((language) => {
+          if (!repository.fork && repository.description && (language && language.data)) {
+            let demo_link = repository.has_pages ? `https://jmaicaaan.github.io/${repository.name}` : '';
+            return {
+              name: repository.name,
+              description: repository.description,
+              url: repository.html_url,
+              demo_link,
+              language: language.data
+            }
+          }
+        });
+    })
+    .then((res) => {
+      return res.filter((repos) => {
+        return repos && Object.keys(repos).length
+      });
     });
-    return data;
   }
   throw 'No data has found';
+}
+
+function getRepoLanguage(repositoryName) {
+  if (!repositoryName) {
+    return;
+  }
+  return github.repos.getLanguages({ owner: 'jmaicaaan', repo: repositoryName })
+    .then((language) => language);
 }
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
